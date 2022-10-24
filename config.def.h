013d7d72dc083cd7c03e9cf0bbe85bbff9cf9250
Zaiqiang Dong <zaiqdong@didiglobal.com>
2022-10-24 09:39:34 +0800

    add config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:style=bold:size=12";
+static char *font = "Hack Nerd Font Mono:style=bold:size=11";
 static int borderpx = 2;
 
 /*
