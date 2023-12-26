5853f684832e45ed4ef265e0d228ce5ce2ed449f
Zaiqiang Dong <zaiqdong@didiglobal.com>
2023-12-26 23:46:21 +0800

    add config

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrains Mono NL:style=Bold:size=13";
-static char *font2[] = { "JetBrainsMonoNL Nerd Font:style=Bold:size=12",};
+static char *font2[] = { "Noto Color Emoji:style=Regular:size=11",};
 
 static int borderpx = 2;
 
